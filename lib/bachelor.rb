c5d9450744b2543ca4e9da9b4a5a90731341ef3d
estein1988 <estein1988@gmail.com>
2020-08-05 16:47:35 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
+require 'json'
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  data.each do |term, contestents|
+    if term == season
+      contestents.each do |info|
+        if info["status"] == "Winner"
+          return info["name"].split(" ")[0]
+        end 
+      end 
+    end 
+  end
+end 
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |term, contestents|
+    contestents.each do |info|
+      if info["occupation"] == occupation 
+        return info["name"]
+      end 
+    end 
+  end       
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+  counter = 0
+  data.each do |term, contestents|
+    contestents.each do |info|
+      if info["hometown"] == hometown  
+        counter += 1
+      end
+    end
+  end
+  counter 
+end 
+
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |term, contestents|
+    contestents.each do |info|
+      if info["hometown"] == hometown
+        return info["occupation"]
+      end 
+    end 
+  end   
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each do |term, contestents|
+    if term == season
+      ages = contestents.map do |info| 
+        info["age"]
+      end
+      total_ages = ages.reduce(0) do |sum, n|
+        sum + n.to_f
+      end 
+      return (total_ages / ages.length).round()
+    end 
+  end 
 end
